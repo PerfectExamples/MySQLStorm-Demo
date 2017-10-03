6ae0380ff23408269e439d7c8abc2ea4373a02d6
Jonathan Guthrie <jono@perfect.org>
2017-10-03 15:30:08 -0400

    update for swift 4/perfect 3

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-01-04 16:25:29 +0000
+// Date: 2017-10-03 19:25:12 +0000
 import PackageDescription
 let package = Package(
-    name: "MySQLStORM-Demo",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", majorVersion: 1, minor: 0),
-    ]
+	name: "MySQLStORM-Demo",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", majorVersion: 3),
+	]
 )
